f776aeb6e1038989c562582be2b119c7ea2869b7
Chris <63784760+chk-code@users.noreply.github.com>
2020-05-26 19:13:02 +0200

    fix: environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://64700F68CB54EE3F3E6476EC72CCABBF.gr7.eu-central-1.eks.amazonaws.com:8080/api/v0'
 };
 
 /*
